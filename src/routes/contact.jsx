3d439716e9ac8c399c237b8684e54af7ff88cf6a
afhorton <106110920+afhorton@users.noreply.github.com>
2023-11-07 09:51:00 -0700

    Here

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,5 +1,12 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import { Form, useLoaderData, useFetcher } from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
+
+export async function action({ request, params }) {
+    let formData = await request.formData();
+    return updateContact(params.contactId, {
+        favorite: formData.get("favorite") === "true", }
+        );
+}
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
@@ -67,10 +74,13 @@ const { contact } = useLoaderData();
 }
 
 function Favorite({ contact }) {
-  // yes, this is a `let` for later
+  const fetcher = useFetcher();
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === true;
+  }
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -78,6 +88,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
